88e1071c3e73514dc5e571546457ba6d68b58f21
Thales O Arakawa <thales.arakawa2@gmail.com>
2022-08-05 00:49:39 -0300

    signalTreatment

--- sensorIrSharp/std.ps1
+++ sensorIrSharp/std.ps1
@@ -1,13 +1,16 @@
 $localPath = "C:\Users\Cheech\Documents\TCC\sensorIrSharp\sample 4-30\filtered\"
+$avgPath = "C:\Users\Cheech\Documents\TCC\sensorIrSharp\avg.txt"
 
 $files = Get-ChildItem -Name $localPath
+$avg = Get-Content -Path $avgPath
 
 for ($i = 0; $i -lt $files.Length; $i++) {
     #Write-Host ($localPath + "trat" + ($i + 4))
     $content = Get-Content ($localPath + "trat" + ($i + 4) + ".txt")
     $sum = 0
     for ($j = 0; $j -lt $content.Length; $j++) {
-        $sum += $content[$j].ToInt16($aux)
+        $sum += [Math]::Pow(($content[$j].ToDecimal($aux) - $avg[$i].ToDecimal($aux1)), 2)
     }
-    ("$($i + 4)" + "`t" + "$($sum / $content.Length)") >> avg.txt
+    [Math]::Sqrt(($sum / $content.Length)) >> std.txt
+
 }
