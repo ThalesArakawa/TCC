c6802d4aa7b8737f56811daad5cd0acdb832da15
Thales O Arakawa <thales.arakawa2@gmail.com>
2022-09-21 12:57:40 -0300

    update2-2192022

---
+++ Código/LabRem_2022_09_21_15_52_38/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(windowWidth, 400);
+    cenario = new Cenario();
+  }
+  function draw() {
+  
+    background(255);
+    print(windowWidth, windowHeight);
+  
+  
+    cenario.windowResponse(windowWidth, windowHeight);
+    
+    rotate((2*PI)*0.01);
+    cenario.circle(200);
+    cenario.rectangle();
+  }
+  
